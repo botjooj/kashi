8bbee648c72717a526ae89da1886f2d05162ed85
botjooj <76960365+botjooj@users.noreply.github.com>
2021-02-28 18:45:13 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,180 @@
+const help = (prefix) => {
+	return `
+「 *BOT TRADUZIDO POR MR KAKASHI* 」
+
+◪ *INFO*
+  ❏ Prefixo: 「  ${prefix}  」
+  ❏ Criador: MR KAKASHI
+◪ *SOBRE* 
+  │
+  1├─ ❏ ${prefix}instagram : OFF
+
+  2├─ ❏ ${prefix}blocklist
+  3├─ ❏ ${prefix}grupo whatsapp : https://chat.whatsapp.com/K6X6sVsfkCp1KZdIIuU4Kk
+  4├─ ❏ ${prefix} numero whatsapp wa.me/558699541889
+  5└─ ❏ ${prefix}telegram : OFF
+◪ *MÍDIA* 
+  │
+  6├─ ❏ ${prefix}sticker
+  7├─ ❏ ${prefix}stickergif  
+  8├─ ❏ ${prefix}toimg       
+  9├─ ❏ ${prefix}tomp3
+  10├─ ❏ ${prefix}snowwrite
+  11├─ ❏ ${prefix}3dtext
+  12├─ ❏ ${prefix}neonlogo
+  13├─ ❏ ${prefix}neonlogo2
+  14├─ ❏ ${prefix}lionlogo
+  15├─ ❏ ${prefix}jokerlogo
+  16├─ ❏ ${prefix}shadow
+  17├─ ❏ ${prefix}burnpaper
+  18├─ ❏ ${prefix}coffee
+  19├─ ❏ ${prefix}lovepaper
+  20├─ ❏ ${prefix}woodblock
+  21├─ ❏ ${prefix}qowheart
+  22├─ ❏ ${prefix}mutgrass
+  23├─ ❏ ${prefix}undergocean
+  24├─ ❏ ${prefix}woodenboards
+  25├─ ❏ ${prefix}wolfmetal
+  26├─ ❏ ${prefix}metalictglow
+  27├─ ❏ ${prefix}8bit
+  28├─ ❏ ${prefix}harrypotter
+  29├─ ❏ ${prefix}pubglogo
+  30└─ ❏ ${prefix}quotemaker
+◪ *MEIOS DE COMUNICAÇÃO*
+  │
+  31├─ ❏ ${prefix}trendtwit
+  32├─ ❏ ${prefix}randomkpop
+  33└─ ❏ ${prefix}ytsearch   
+◪ *EDUCAÇÃO*
+  │
+  34├─ ❏ ${prefix}wiki
+  35├─ ❏ ${prefix}wikien
+  36├─ ❏ ${prefix}nulis
+  37├─ ❏ ${prefix}quotes
+  38├─ ❏ ${prefix}quotes2
+  39└─ ❏ ${prefix}artinama
+◪ *MAGIC SHELL*
+  │
+  40├─ ❏ ${prefix}apakah
+  41├─ ❏ ${prefix}kapankah
+  42├─ ❏ ${prefix}rate
+  43└─ ❏ ${prefix}bisakah
+◪ *DOWNLOADS*
+  44├─ ❏ ${prefix}pinterest
+  45├─ ❏ ${prefix}ytmp4
+  46├─ ❏ ${prefix}tiktok
+  47└─ ❏ ${prefix}joox
+◪ *MEME*
+  │
+  48├─ ❏ ${prefix}meme
+  49└─ ❏ ${prefix}memeindo
+◪ *GRUPO*
+  │
+  50├─ ❏ ${prefix}opengc
+  51├─ ❏ ${prefix}closegc
+  52├─ ❏ ${prefix}promote
+  53├─ ❏ ${prefix}demote
+  54├─ ❏ ${prefix}tagall
+  55├─ ❏ ${prefix}tagall2
+  56├─ ❏ ${prefix}tagall3
+  57├─ ❏ ${prefix}tagall4
+  58├─ ❏ ${prefix}tagall5
+  59├─ ❏ ${prefix}add
+  60├─ ❏ ${prefix}kick
+  61├─ ❏ ${prefix}listadmins
+  62├─ ❏ ${prefix}linkgroup
+  63├─ ❏ ${prefix}leave
+  64├─ ❏ ${prefix}welcome
+  65├─ ❏ ${prefix}nsfw
+  66├─ ❏ ${prefix}leveling
+  67├─ ❏ ${prefix}level
+  68├─ ❏ ${prefix}delete
+  69├─ ❏ ${prefix}ownergroup
+  70└─ ❏ ${prefix}criador
+╚═════════════╝
+◪ *SONS*
+  │
+  71├─ ❏ ${prefix}play 
+  72└─ ❏ ${prefix}tts
+╚═════════════╝
+◪ *MUSICA*
+  │
+  73├─ ❏ ${prefix}lirik
+  74└─ ❏ ${prefix}chord
+╚═════════════╝
+◪ *ISLAM*
+  │
+  75└─ ❏ ${prefix}quran
+╚═════════════╝
+◪ *STALK*
+  │
+  76├─ ❏ ${prefix}tiktokstalk
+  77└─ ❏ ${prefix}igstalk
+╚═════════════╝
+◪ *ANIME*
+  │
+  78├─ ❏ ${prefix}neonime
+  79├─ ❏ ${prefix}pokemon
+  80├─ ❏ ${prefix}loli
+  81├─ ❏ ${prefix}waifu
+  82├─ ❏ ${prefix}randomanime
+  83├─ ❏ ${prefix}husbu
+  84├─ ❏ ${prefix}husbu2
+  85├─ ❏ ${prefix}wait
+  86└─ ❏ ${prefix}nekonime
+╚═════════════╝
+◪ *18+*  pornô anime
+  |
+  87├─ ❏ ${prefix}randomhentai
+  88├─ ❏ ${prefix}nsfwtrap          
+  89└─ ❏ ${prefix}nsfwneko  
+  ╚═════════════╝      
+◪ *DIVERSÃO*
+  │
+  90├─ ❏ ${prefix}alay
+  91├─ ❏ ${prefix}gantengcek
+  92├─ ❏ ${prefix}watak
+  93├─ ❏ ${prefix}hobby
+  94├─ ❏ ${prefix}game
+  95├─ ❏ ${prefix}bucin
+  96├─ ❏ ${prefix}trust
+  97├─ ❏ ${prefix}dare
+  98└─ ❏ ${prefix}simi
+╚═════════════╝
+◪ *INFORMAÇÕES*
+  │
+  99├─ ❏ ${prefix}bahasa 
+  100├─ ❏ ${prefix}kodenegara
+  101├─ ❏ ${prefix}kbbi
+  102├─ ❏ ${prefix}fakta
+  103├─ ❏ ${prefix}infocuaca
+  104├─ ❏ ${prefix}infogempa
+  105├─ ❏ ${prefix}jadwaltvnow
+  106└─ ❏ ${prefix}covid
+╚═════════════╝
+◪ *SOMENTE DONO DO BOT*
+  │
+  107├─ ❏ ${prefix}setprefix
+  108├─ ❏ ${prefix}block
+  109├─ ❏ ${prefix}bc
+  110├─ ❏ ${prefix}bcgc 
+  111├─ ❏ ${prefix}clone   
+  112└─ ❏ ${prefix}clearall
+╚═════════════╝
+◪ *OUTROS*
+  │
+  113├─ ❏ ${prefix}send     
+  114├─ ❏ ${prefix}wame  
+  115├─ ❏ ${prefix}virtex    
+  116├─ ❏ ${prefix}exe
+  117├─ ❏ ${prefix}qrcode  
+  118├─ ❏ ${prefix}afk
+  119├─ ❏ ${prefix}timer
+  120├─ ❏ ${prefix}fml
+  121└─ ❏ ${prefix}fml2
+╚═════════════╝
+◪ *AH KAKASHI NÉ?🙄🤙*
+`
+}
+
+exports.help = help
